31eb79577f8d862679ae3af9edb8ab11d1b0ea2f
albardoo02 <albardoo02@gmail.com>
2022-12-26 15:14:16 +0900

    1.3.0

--- src/main/java/com/github/albardoo02/ChatLimit.java
+++ src/main/java/com/github/albardoo02/ChatLimit.java
@@ -13,50 +13,62 @@ import java.io.File;
 import java.io.IOException;
 
 public final class ChatLimit extends JavaPlugin {
+    public String version = getDescription().getVersion();
+    public String prefix = ChatColor.translateAlternateColorCodes('&',getConfig().getString("message.prefix"));
 
-    public String prefix = ChatColor.translateAlternateColorCodes('&',getConfig().getString("prefix"));
 
-    private File MessageConfigFile;
-    private FileConfiguration MessageConfig;
+    private File WordConfigFile;
+    private FileConfiguration WordConfig;
+
     @Override
     public void onEnable() {
-        super.onEnable();
-        getLogger().info(ChatColor.translateAlternateColorCodes('&',"&bChatLimit &3v" + getConfig().getString("version")));
-        getLogger().info(ChatColor.translateAlternateColorCodes('&',"NGワード検出時にOPに通知: &a" + getConfig().getString("MessageToOP.Enabled")));
-        getLogger().info(ChatColor.translateAlternateColorCodes('&',"NGコマンド検出時にOPに通知: &a" + getConfig().getString("CommandToOP.Enabled")));
-        this.saveDefaultConfig();
-        createMessageConfig();
+        getLogger().info("");
+        getLogger().info(ChatColor.translateAlternateColorCodes('&',"&cChatLimit &bv" + version));
+        if (getConfig().getString("Chat.Enabled").equals("true")) {
+            getLogger().info(ChatColor.translateAlternateColorCodes('&', "チャット制限: &atrue"));
+        }
+        else {
+            getLogger().info(ChatColor.translateAlternateColorCodes('&', "チャット制限: &cfalse"));
+        }
+        if (getConfig().getString("Command.Enabled").equals("true")) {
+            getLogger().info(ChatColor.translateAlternateColorCodes('&', "コマンド制限: &atrue"));
+        }
+        else {
+            getLogger().info(ChatColor.translateAlternateColorCodes('&', "コマンド制限: &cfalse"));
+        }
+        getLogger().info("");
 
+        saveDefaultConfig();
+        createWordConfig();
         getServer().getPluginManager().registerEvents(new PlayerChat(this),this);
-        getServer().getPluginManager().registerEvents(new PlayerCommandPreprocess(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerCommandPreprocess(this),this);
 
         getCommand("chatlimit").setExecutor(new ChatLimitCommand(this));
     }
 
-    public FileConfiguration getMessageConfig() {
-        return this.MessageConfig;
+    @Override
+    public void onDisable() {
+        getLogger().info("プラグインが停止しました");
+        getLogger().info("Goodbye!");
     }
 
-    private void createMessageConfig() {
-        MessageConfigFile = new File(getDataFolder(), "message.yml");
-        if (!MessageConfigFile.exists()) {
-            MessageConfigFile.getParentFile().mkdirs();
-            saveResource("message.yml", false);
+    public FileConfiguration getWordConfig() {
+        return this.WordConfig;
+    }
+
+    private void createWordConfig() {
+        WordConfigFile = new File(getDataFolder(), "word.yml");
+        if (!WordConfigFile.exists()) {
+            WordConfigFile.getParentFile().mkdirs();
+            saveResource("word.yml", false);
         }
 
-        MessageConfig = new YamlConfiguration();
+        WordConfig = new YamlConfiguration();
         try {
-            MessageConfig.load(MessageConfigFile);
+            WordConfig.load(WordConfigFile);
         } catch (IOException | InvalidConfigurationException e) {
             e.printStackTrace();
         }
     }
 
-    @Override
-    public void onDisable() {
-        super.onDisable();
-        saveConfig();
-        getLogger().info("Plugin has been disabled");
-        getLogger().info("Goodbye!");
-    }
 }
--- src/main/java/com/github/albardoo02/listener/PlayerCommandPreprocess.java
+++ src/main/java/com/github/albardoo02/listener/PlayerCommandPreprocess.java
@@ -1,9 +1,11 @@
 package com.github.albardoo02.listener;
 
 import com.github.albardoo02.ChatLimit;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,29 +18,47 @@ public class PlayerCommandPreprocess implements Listener {
     ChatLimit plugin;
     public PlayerCommandPreprocess(ChatLimit plugin){
         this.plugin = plugin;
+
     }
 
     @EventHandler
-    public void onCommandPreprocess(PlayerCommandPreprocessEvent event){
+    public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
-        if (!plugin.getConfig().getBoolean("CommandToOP.Enabled")){
+        if (!plugin.getConfig().getBoolean("Command.Enabled")) {
             return;
         }
-        if(player.hasPermission("ChatLimit.command.bypass")){
+        if (player.hasPermission("ChatLimit.command.bypass")) {
             return;
         }
-        List<String> cmd = plugin.getMessageConfig().getStringList("KickCommands");
-        for(String command : cmd){
-            if(event.getMessage().equalsIgnoreCase("/" + command)){
-                event.setCancelled(true);
+        List<String> cmd = plugin.getWordConfig().getStringList("Command");
+        for (String command : cmd) {
+            String reason = ChatColor.translateAlternateColorCodes('&',plugin.getConfig().getString("Command.Kick.Reason").replace("%player",player.getName()).replace("%command","/" + command));
+            String notice = ChatColor.translateAlternateColorCodes('&',plugin.prefix + " &8&l» "+ plugin.getConfig().getString("Command.SendToOP.Notice").replace("%player",player.getName()).replace("%command","/" + command));
+            String console = ChatColor.translateAlternateColorCodes('&',plugin.prefix + " &8&l» "+ plugin.getConfig().getString("Command.SendToConsole.Notice").replace("%player",player.getName()).replace("%command","/" + command));
+            if (event.getMessage().equalsIgnoreCase("/" + command)) {
+                if (!plugin.getConfig().getBoolean("Command.SendToOP.Enabled")) {
+                    return;
+                }
                 for (Player ops : Bukkit.getServer().getOnlinePlayers()) {
-                    if(ops.isOp()){
-                        ops.sendMessage(ChatColor.translateAlternateColorCodes('&',plugin.prefix + " &8&l» &r" + plugin.getConfig().getString("CommandToOP.message").replace("%player",event.getPlayer().getName()).replace("%command",event.getMessage())));
-                        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',plugin.prefix + " &8&l» &r" + plugin.getConfig().getString("CommandToOP.message").replace("%player",event.getPlayer().getName()).replace("%command",event.getMessage())));
+                    if (ops.isOp()) {
+                        TextComponent Message = new TextComponent(notice);
+                        Message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("§c検出されたコマンド: §e/" + command + "\n§fConfigで設定を変更できます").create()));
+                        ops.spigot().sendMessage(Message);
                     }
                 }
+                if (!plugin.getConfig().getBoolean("Command.SendToConsole.Enabled")) {
+                    return;
+                }
+                Bukkit.getServer().getConsoleSender().sendMessage(console);
+                if (!plugin.getConfig().getBoolean("Command.Cancel.Enabled")) {
+                    return;
+                }
+                event.setCancelled(true);
+                if (!plugin.getConfig().getBoolean("Command.Kick.Enabled")) {
+                    return;
+                }
+                player.kickPlayer(reason);
             }
         }
     }
-
 }
